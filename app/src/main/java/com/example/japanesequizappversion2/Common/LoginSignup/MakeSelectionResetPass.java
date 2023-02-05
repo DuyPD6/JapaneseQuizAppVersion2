f060bfc584524682d0f3519b98337e343b6274cf
bunny <bunnyjumb@gmail.com>
2023-02-06 01:29:49 +0700

    Reset password screen

---
+++ app/src/main/java/com/example/japanesequizappversion2/Common/LoginSignup/MakeSelectionResetPass.java
@@ -0,0 +1,16 @@
+package com.example.japanesequizappversion2.Common.LoginSignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.japanesequizappversion2.R;
+
+public class MakeSelectionResetPass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_selection_reset_pass);
+    }
+}
